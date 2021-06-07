0c67cb2b41e1527d07acbddbfd89c36d7c1868ab
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2021-06-07 12:37:53 +0200

    Add files via upload

--- api/db/db.php
+++ api/db/db.php
@@ -2,10 +2,11 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "rootroot";
-	$database = "hotelmascotas6";
+	$database = "hotelmascotas8";
 
 	try {
-		$conexion = new PDO("mysql:host=$servername;dbname=$database", $username, $password); 	 	 	 	 	 	
+		$conexion = new PDO("mysql:host=$servername;dbname=$database", $username, $password, 
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 	 	 	 	 	 	
 		$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 	 	 	 	 	 	
 	} catch (PDOException $ex) {
 		echo $ex->getMessage(); 	 	 	 	 	 	
